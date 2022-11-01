5759ac09ace5845f5fad307cdc02a9a84c51badf
koutsosg <g.koutsos123@gmail.com>
2022-11-01 17:11:01 +0100

    functional button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "reactstrap";
+
 
 function App() {
+  const quotesArrUrl =
+    "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+
+  const [quote, setQuote] = useState("me");
+
+  const [author, setAuthor] = useState("");
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  const fetchQuote = () => {
+    let url = quotesArrUrl;
+    fetch(url)
+      .then((response) => response.json())
+      .then((arr) => {
+        let quotesArr = arr.quotes;
+        let randomInt = Math.floor(quotesArr.length * Math.random());
+        let randomQuote = quotesArr[randomInt];
+
+        setQuote(randomQuote.quote);
+        setAuthor(randomQuote.author);
+      });
+  };
+  const handleClick = () => {
+    fetchQuote();
+  };
+
+
+  const tweet = `${quote} \n --${author}`;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="quote-box">
+          <p id="text">"{quote}"</p>
+          <p id="author"> -{author}</p>
+          <a
+            id="tweet-quote"
+            className="btn btn-social-icon btn-twitter"
+            target="_blank"
+            rel="noreferrer"
+            href={encodeURI(`http://twitter.com/intent/tweet?text=${tweet}`)}
+          >
+            <i className="fa fa-twitter"></i>
+          </a>
+          <Button id="new-quote" onClick={handleClick}>
+            New Quote
+          </Button>
+        </div>
       </header>
     </div>
   );
